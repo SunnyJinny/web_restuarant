fc53c52e489abc1b4c9a9417b8f585776b0fab5a
SunnyJinny <53663147+SunnyJinny@users.noreply.github.com>
2020-04-04 12:50:53 +0200

    Add files via upload

---
+++ src/components/OneMenu.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="m-element">
+        <img class="m-img" :src="imgInfo.src">
+        <h3 class="m-title">{{ mtitle }}</h3>
+        <span class="m-price">{{ mprice }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "OneMenu",
+    props: ["imgInfo", "mtitle", "mprice"]
+};
+</script>
+
+<style scoped>
+
+.m-element {
+    width: 10rem;
+    /*height: 15rem;*/
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+    justify-content: center;
+    align-items: center;
+}
+.m-img {
+    width: 10rem;
+    height: 10rem;
+    border-radius: 50%;
+    object-fit: cover;
+}
+.m-title {
+    font-size: 1.5rem;
+}
+.m-price {
+    font-size: 1.25rem;
+}
+@media only screen and (max-width: 900px) {
+    .m-title {font-size: 1rem; margin-bottom: 0;}
+    .m-price {font-size: 0.75rem;}
+}
+
+/*
+div {
+        border: 1px solid #000;
+    }
+*/
+</style>
---
+++ src/components/Order.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Order page</div>
+</template>
+
+<script>
+    export default {
+        name: "Order"
+    };
+</script>
+
+<style scoped>
+    div {
+        height: 100vh;
+        border: 1px solid #000;
+    }
+</style>
