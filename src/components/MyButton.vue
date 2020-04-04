6583bddc240d5474ad010913fd21714c9ddd7da2
SunnyJinny <53663147+SunnyJinny@users.noreply.github.com>
2020-04-04 12:50:39 +0200

    Create MyButton.vue

---
+++ src/components/MyButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="btn-wrap">
+        <button>{{ btntitle }}
+            </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [ "btntitle" ],
+        data: function () {
+            return {
+            }
+
+        }
+    };
+
+</script>
+
+<style scoped>
+.btn-wrap {
+    text-align: center;
+}
+</style>
